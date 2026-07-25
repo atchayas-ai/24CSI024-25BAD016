7609bb625ef87434e1c3c75836d4aa066a0c0d95
atchayas-ai <atchayas.personal@gmail.com>
2026-07-25 21:53:36 +0530

    Update Linear Search.c

--- Linear Search.c
+++ Linear Search.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
-
 int main() {
-    int N, K;
-    scanf("%d %d", &N, &K);
-
-    int arr[N], deque[N];
-    for (int i = 0; i < N; i++)
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter array elements: ");
+    for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-
-    int front = 0, rear = -1;
-
-    for (int i = 0; i < N; i++) {
-
-        while (front <= rear && deque[front] <= i - K)
-            front++;
-
-        while (front <= rear && arr[deque[rear]] <= arr[i])
-            rear--;
-
-        deque[++rear] = i;
-
-        if (i >= K - 1)
-            printf("%d ", arr[deque[front]]);
     }
-
+    int e;
+    printf("Enter an element to be found: ");
+    scanf("%d", &e);
+    int flag = 0;
+    int index;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == e) {
+            index = i;
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 1)
+        printf("Element found at index %d", index);
+    else
+        printf("Element not found");
     return 0;
 }
